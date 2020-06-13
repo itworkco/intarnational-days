283483e9e7f087d9e0d7a7f12bf3c58e0eb34029
mahmoudgeater <zidane_hoda2200@yahoo.com>
2020-06-13 21:50:10 +0200

    month and day select function

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool selected = false;
+  var day = DateTime.now().day;
+  var monthNumber = DateTime.now().month;
 
   List<String> monthes = [
     'يناير',
@@ -37,7 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
     'مارس',
     'أبريل',
     'مايو',
-    'ييونيو',
+    'يونيو',
     'يوليو',
     'أغسطس',
     'سبتمبر',
@@ -67,6 +68,13 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  getMonth(index) {
+    setState(() {
+      monthNumber = index + 1;
+      print(monthNumber);
+    });
+  }
+
   @override
   void initState() {
     addModel();
@@ -80,12 +88,13 @@ class _MyHomePageState extends State<MyHomePage> {
         elevation: 0,
         backgroundColor: Color(0xffFBFCFC),
         leading: IconButton(
-            icon: Icon(
-              Icons.search,
-              color: Color(0xff4EA1B5),
-              size: 30,
-            ),
-            onPressed: null),
+          icon: Icon(
+            Icons.search,
+            color: Color(0xff4EA1B5),
+            size: 30,
+          ),
+          onPressed: () {},
+        ),
         title: Text(
           'أيام دولية',
           style: TextStyle(
@@ -103,36 +112,39 @@ class _MyHomePageState extends State<MyHomePage> {
           )
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: MediaQuery.of(context).size.height * .45,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/monthbg.png'),
-                    fit: BoxFit.cover,
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      offset: Offset(2, 4),
-                      color: Color(0xff00000029),
-                      blurRadius: 10,
-                    ),
-                  ]),
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
-                child: Container(
-                    decoration:
-                        BoxDecoration(color: Colors.white.withOpacity(0)),
-                    child: GridView.builder(
-                      padding: EdgeInsets.all(14),
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 4),
-                      itemBuilder: (_, index) => Container(
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height * .45,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/monthbg.png'),
+                fit: BoxFit.cover,
+              ),
+//              boxShadow: [
+//                BoxShadow(
+//                  offset: Offset(1, 1),
+//                  color: Color(0xff00000029),
+//                  blurRadius: 2,
+//                ),
+//              ],
+            ),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+              child: Container(
+                  decoration: BoxDecoration(color: Colors.white.withOpacity(0)),
+                  child: GridView.builder(
+                    padding: EdgeInsets.all(14),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4),
+                    itemBuilder: (_, index) => GestureDetector(
+                      onTap: () {
+                        getMonth(index);
+                      },
+                      child: Container(
                         decoration: BoxDecoration(
                           image: DecorationImage(
-                            image: selected
+                            image: (monthNumber == index + 1)
                                 ? AssetImage('assets/images/selectedCard.png')
                                 : AssetImage('assets/images/monthCard.png'),
                           ),
@@ -141,147 +153,196 @@ class _MyHomePageState extends State<MyHomePage> {
                           child: Text(
                             monthes[index],
                             style: TextStyle(
-                              color: Color(0xff37352F),
+                              color: (monthNumber == index + 1)
+                                  ? Color(0xffffffff)
+                                  : Color(0xff37352F),
                               fontSize: 15,
                               fontFamily: 'SukarBold',
                             ),
                           ),
                         ),
                       ),
-                      itemCount: monthes.length,
-                    )),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
-              padding: EdgeInsets.only(left: 20, right: 8),
-              child: Stack(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(right: 54),
-                    width: 1,
-                    height: MediaQuery.of(context).size.height * .37,
-                    color: Color(0xffF3F3F3),
-                  ),
-                  Container(
-                    child: ListView.builder(
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                      itemCount: eventModel.length,
-                      itemBuilder: (context, index) {
-                        return eventRow(index);
-                      },
                     ),
-                  )
-                ],
+                    itemCount: monthes.length,
+                  )),
+            ),
+          ),
+          Expanded(
+            child: Stack(children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(right: 64),
+                width: 1,
+                height: MediaQuery.of(context).size.height * .40,
+                color: Color(0xffF3F3F3),
               ),
-            )
-          ],
-        ),
+              ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: eventModel.length,
+                itemBuilder: (context, index) {
+                  return eventRow(index);
+                },
+              ),
+            ]),
+          )
+        ],
       ),
     );
   }
 
   eventRow(index) {
-    return Container(
-      padding: EdgeInsets.only(bottom: 6),
-      margin: EdgeInsets.only(top: 25),
-      child: Row(
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              Text(
-                eventModel[index].number,
-                style: TextStyle(
-                  fontFamily: 'Sukarblack',
-                  fontSize: 25,
-                  color: Color(0xff191818),
+    return Padding(
+      padding: const EdgeInsets.only(top: 10, right: 10, left: 10),
+      child: Container(
+        margin: EdgeInsets.only(top: 10),
+        child: Row(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Text(
+                  eventModel[index].number,
+                  style: TextStyle(
+                    fontFamily: 'Sukarblack',
+                    fontSize: 25,
+                    color:
+                        (day == index) ? Color(0xff1E824c) : Color(0xff191818),
+                  ),
                 ),
-              ),
-              Text(
-                eventModel[index].day,
-                style: TextStyle(
-                  fontFamily: 'Sukarblack',
-                  fontSize: 10,
-                  color: Color(0xff707070),
+                Text(
+                  eventModel[index].day,
+                  style: TextStyle(
+                    fontFamily: 'Sukarblack',
+                    fontSize: 10,
+                    color:
+                        (day == index) ? Color(0xff1E824c) : Color(0xff191818),
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Container(
-              width: 13,
-              height: 13,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5.0),
-                color: Color(0xffF3F3F3),
-              ),
-              child: Center(
-                child: Container(
-                  width: 5,
-                  height: 5,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5.0),
-                    color: Color(0xff707070),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Container(
+                width: 13,
+                height: 13,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5.0),
+                  color: Color(0xffF3F3F3),
+                ),
+                child: Center(
+                  child: Container(
+                    width: 5,
+                    height: 5,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5.0),
+                      color: (day == index)
+                          ? Color(0xff1E824c)
+                          : Color(0xff191818),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) {
-                    return Details();
-                  }),
-                );
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular(50),
-                      topRight: Radius.circular(50),
-                      topLeft: Radius.circular(7),
-                      bottomLeft: Radius.circular(7)),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(.3),
-                      blurRadius: 6,
-                      offset: Offset(0, 0),
-                    ),
-                  ],
-                ),
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage(eventModel[index].image),
+            Expanded(
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) {
+                      return Details();
+                    }),
+                  );
+                },
+//                onTap: () {
+//                  setState(() {
+//                    day = index;
+//                  });
+//                },
+                child: (day == index)
+                    ? Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(7),
+                              topRight: Radius.circular(7),
+                              topLeft: Radius.circular(7),
+                              bottomLeft: Radius.circular(7)),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(.3),
+                              blurRadius: 6,
+                              offset: Offset(0, 0),
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage(eventModel[index].image),
+                                ),
+                              ),
+                              height: 61,
+                              width: 61,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                eventModel[index].title,
+                                style: TextStyle(
+                                  color: Color(0xff191818),
+                                  fontSize: 16,
+                                  fontFamily: 'SukarBold',
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    : Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(50),
+                              topRight: Radius.circular(50),
+                              topLeft: Radius.circular(7),
+                              bottomLeft: Radius.circular(7)),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(.3),
+                              blurRadius: 6,
+                              offset: Offset(0, 0),
+                            ),
+                          ],
+                        ),
+                        child: Row(
+                          children: <Widget>[
+                            Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage(eventModel[index].image),
+                                ),
+                              ),
+                              height: 61,
+                              width: 61,
+                            ),
+                            SizedBox(width: 4),
+                            Expanded(
+                              child: Text(
+                                eventModel[index].title,
+                                style: TextStyle(
+                                  color: Color(0xff191818),
+                                  fontSize: 18,
+                                  fontFamily: 'SukarBold',
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      height: 61,
-                      width: 61,
-                    ),
-                    SizedBox(width: 4),
-                    Expanded(
-                      child: Text(
-                        eventModel[index].title,
-                        style: TextStyle(
-                          color: Color(0xff191818),
-                          fontSize: 18,
-                          fontFamily: 'SukarBold',
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
